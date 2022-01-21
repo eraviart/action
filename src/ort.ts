3a015a814059bb175726aafed5a0e26af5646285
Emmanuel Raviart <emmanuel@raviart.com>
2022-01-21 16:10:41 +0100

    Don't report using Opossum format, because it is not available.

--- src/ort.ts
+++ src/ort.ts
@@ -52,7 +52,7 @@ export async function report(output_dir: string): Promise<void> {
     const ort_args = [
         'report',
         '-f',
-        'Opossum,SpdxDocument',
+        'SpdxDocument',
         '-i',
         '/out/evaluation-result.yml',
         '-o',
